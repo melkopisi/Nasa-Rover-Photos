af0dfb5a9103766f91e1a17858672092eea3723c
Kopisi <m.elkopisi@gmail.com>
2022-10-13 16:54:51 +0200

    added viewbinding feature and initialized dagger hilt

--- presentation/src/main/java/me/melkopisi/nasaroverphotos/MainActivity.kt
+++ presentation/src/main/java/me/melkopisi/nasaroverphotos/MainActivity.kt
@@ -2,10 +2,13 @@ package me.melkopisi.nasaroverphotos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import me.melkopisi.nasaroverphotos.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(ActivityMainBinding.inflate(layoutInflater).root)
   }
 }
