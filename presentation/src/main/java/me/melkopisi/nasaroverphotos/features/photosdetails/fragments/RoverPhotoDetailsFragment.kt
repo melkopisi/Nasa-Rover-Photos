6c7fed1640a99aea17a802f0f282e30f93e8761d
Kopisi <m.elkopisi@gmail.com>
2022-10-14 16:39:21 +0200

    some enhancements in rover details ui items

--- presentation/src/main/java/me/melkopisi/nasaroverphotos/features/photosdetails/fragments/RoverPhotoDetailsFragment.kt
+++ presentation/src/main/java/me/melkopisi/nasaroverphotos/features/photosdetails/fragments/RoverPhotoDetailsFragment.kt
@@ -7,6 +7,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import androidx.core.text.color
 import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.AppBarLayout
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,7 +34,7 @@ class RoverPhotoDetailsFragment : Fragment() {
   private var _binding: FragmentRoverPhotoDetailsBinding? = null
   private val binding get() = _binding!!
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
     _binding = FragmentRoverPhotoDetailsBinding.inflate(inflater, container, false)
     return binding.root
   }
@@ -43,33 +47,34 @@ class RoverPhotoDetailsFragment : Fragment() {
   }
 
   private fun setupToolbar() {
-    with(binding){
-    (requireActivity() as MainActivity).setSupportActionBar(toolbar)
-    (requireActivity() as MainActivity).supportActionBar?.apply {
-      setDisplayHomeAsUpEnabled(true)
-    }
-    toolbar.apply {
-      setNavigationOnClickListener { requireActivity().onBackPressed() }
-      navigationIcon?.setTint(Color.WHITE)
-    }
-    appbarLayout.addOnOffsetChangedListener(object : AppBarStateChangedListener() {
-      override fun onStateChanged(appBarLayout: AppBarLayout, state: State) {
-        binding.roverDetailsToolbarContentLinear.apply {
-          animate().scaleX(if (state == COLLAPSED) 1.0F else 0.97F)
-            .scaleY(if (state == COLLAPSED) 1.0F else 0.97F)
-            .translationY(if (state == COLLAPSED) 0.0F else -20.0F)
-            .alpha(if (state == COLLAPSED) 1.0F else 0.0F)
-            .setDuration(if (state == COLLAPSED) 200L else 50L)
-            .start()
-        }
-        roverDetailsContentCardView.apply {
-          ObjectAnimator.ofFloat(this, "radius", (if (state == COLLAPSED) 0 else 30).toPixel(appBarLayout.context).toFloat())
-            .setDuration(200L)
-            .start()
-        }
+    with(binding) {
+      (requireActivity() as MainActivity).setSupportActionBar(toolbar)
+      (requireActivity() as MainActivity).supportActionBar?.apply {
+        setDisplayHomeAsUpEnabled(true)
       }
-    })
-  }}
+      toolbar.apply {
+        setNavigationOnClickListener { requireActivity().onBackPressed() }
+        navigationIcon?.setTint(Color.WHITE)
+      }
+      appbarLayout.addOnOffsetChangedListener(object : AppBarStateChangedListener() {
+        override fun onStateChanged(appBarLayout: AppBarLayout, state: State) {
+          binding.roverDetailsToolbarContentLinear.apply {
+            animate().scaleX(if (state == COLLAPSED) 1.0F else 0.97F)
+              .scaleY(if (state == COLLAPSED) 1.0F else 0.97F)
+              .translationY(if (state == COLLAPSED) 0.0F else -20.0F)
+              .alpha(if (state == COLLAPSED) 1.0F else 0.0F)
+              .setDuration(if (state == COLLAPSED) 200L else 50L)
+              .start()
+          }
+          roverDetailsContentCardView.apply {
+            ObjectAnimator.ofFloat(this, "radius", (if (state == COLLAPSED) 0 else 30).toPixel(appBarLayout.context).toFloat())
+              .setDuration(200L)
+              .start()
+          }
+        }
+      })
+    }
+  }
 
   private fun setupViews(photoModel: RoverPhotosUiModel?) {
     with(photoModel!!) {
@@ -80,13 +85,36 @@ class RoverPhotoDetailsFragment : Fragment() {
         tvRoverPhotoName.text = imageName
         tvRoverDetailsEarthDateValue.text = earthDate
 
-        tvRoverDetailsCameraShortNameValue.text = getString(R.string.rover_camera_short_name, camera.name)
-        tvRoverDetailsCameraFullNameValue.text = getString(R.string.rover_camera_full_name, camera.fullName)
+        tvRoverDetailsCameraShortNameValue.text = buildSpannedString {
+          bold { append("${getString(R.string.name_title)}: ") }
+          append(camera.name)
+        }
+        tvRoverDetailsCameraFullNameValue.text = buildSpannedString {
+          bold { append("${getString(R.string.full_name_title)}: ") }
+          append(camera.fullName)
+        }
 
-        tvRoverNameValue.text = getString(R.string.rover_name,rover.name)
-        tvRoverStatusValue.text = getString(R.string.rover_status,rover.status)
-        tvRoverLaunchDateValue.text = getString(R.string.rover_launch_date,rover.launchDate)
-        tvRoverLandingDateValue.text = getString(R.string.rover_landing_date,rover.landingDate)
+        tvRoverNameValue.text = buildSpannedString {
+          bold { append("${getString(R.string.name_title)}: ") }
+          append(rover.name)
+        }
+        tvRoverStatusValue.text = buildSpannedString {
+          bold { append("${getString(R.string.status_title)}: ") }
+
+          when (rover.status) {
+            "active" -> color(ContextCompat.getColor(requireContext(), R.color.green)) { append(rover.status) }
+            else -> color(ContextCompat.getColor(requireContext(), R.color.red)) { append(rover.status) }
+          }
+
+        }
+        tvRoverLaunchDateValue.text = buildSpannedString {
+          bold { append("${getString(R.string.rover_launch_date_title)}: ") }
+          append(rover.launchDate)
+        }
+        tvRoverLandingDateValue.text = buildSpannedString {
+          bold { append("${getString(R.string.rover_landing_date_title)}: ") }
+          append(rover.landingDate)
+        }
       }
     }
   }
